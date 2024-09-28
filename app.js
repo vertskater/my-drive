6c1172a5bb6187e34db68cfa902e028ce82c4826
vertskater <vertskater@gmx.at>
2024-09-28 12:18:10 +0200

    init commit, setup project structure and dependecies

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+//init dotenv
+require('dotenv').config();
+//set view engine to EJS
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+//set assets path to public
+app.use(express.static(path.join(__dirname, 'public')))
+//use form-data (req.body)
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT);
